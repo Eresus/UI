8701d95831aaaf83441cb6a0e024ce8720196b16
Михаил Красильников <m.krasilnikov@yandex.ru>
2011-10-19 09:58:59 +0000

    Добработа UI_List_URL_Query

--- src/ui/classes/List/URL/Query.php
+++ src/ui/classes/List/URL/Query.php
@@ -33,12 +33,85 @@
 
 
 /**
- * Шаблон URL с аргументами в запросе
+ * Шаблон URL с аргументами в запросе (query)
+ *
+ * Например, шаблон переключателя страниц может выглядеть так:
+ *
+ * <code>
+ * …/admin.php?mod=ext-myplugin&page=%d
+ * </code>
+ *
+ * Корневой URL может быть задан в {@link __construct конструкторе}.
  *
  * @package UI
  */
 class UI_List_URL_Query implements UI_List_URL_Interface
 {
+	/**
+	 * Базовый URL
+	 *
+	 * Всегда заканчивается символом & или ?
+	 *
+	 * @var string
+	 * @see __construct()
+	 */
+	private $baseURL;
+
+	/**
+	 * Имя аргумента для передачи идентификатора
+	 *
+	 * @var string
+	 * @see setIdName()
+	 */
+	private $idName = 'id';
+
+	/**
+	 * Конструктор
+	 *
+	 * @param string $baseURL  базовый URL, все аргументы будет присоединяться к нему. Если не указан,
+	 *                         будет использован результат вызова {@link WebPage::url()}.
+	 *
+	 * @return UI_List_URL_Query
+	 *
+	 * @since 1.00
+	 * @uses WebPage::url()
+	 */
+	public function __construct($baseURL = null)
+	{
+		if ($baseURL)
+		{
+			$this->baseURL = $baseURL;
+		}
+		else
+		{
+			$this->baseURL = $GLOBALS['page']->url();
+		}
+		$lastChar = mb_substr($this->baseURL, -1);
+		if ('&' != $lastChar && '?' != $lastChar)
+		{
+			$hasQuery = mb_strpos($this->baseURL, '?') !== false;
+			$this->baseURL .= $hasQuery ? '&' : '?';
+		}
+	}
+	//-----------------------------------------------------------------------------
+
+	/**
+	 * Задаёт имя аргумента для передачи идентификатора элмента списка
+	 *
+	 * По умолчанию имя аргумента «id».
+	 *
+	 * @param string $name
+	 *
+	 * @return void
+	 *
+	 * @since 1,.00
+	 */
+	public function setIdName($name)
+	{
+		$this->idName = $name;
+	}
+	//-----------------------------------------------------------------------------
+
 	/**
 	 * Возвращает шаблон URL для переключателя страниц
 	 *
@@ -48,7 +121,7 @@ class UI_List_URL_Query implements UI_List_URL_Interface
 	 */
 	public function getPagination()
 	{
-		return $GLOBALS['page']->url(array('page' => '%d'));
+		return $this->baseURL . 'page=%d';
 	}
 	//-----------------------------------------------------------------------------
 
@@ -63,7 +136,7 @@ class UI_List_URL_Query implements UI_List_URL_Interface
 	 */
 	public function getDelete(UI_List_Item_Interface $item)
 	{
-		return $GLOBALS['page']->url(array('id' => $item->getId(), 'action' => 'delete'));
+		return $this->baseURL . $this->idName . '=' . $item->getId() . '&action=delete';
 	}
 	//-----------------------------------------------------------------------------
 
@@ -78,7 +151,7 @@ class UI_List_URL_Query implements UI_List_URL_Interface
 	 */
 	public function getEdit(UI_List_Item_Interface $item)
 	{
-		return $GLOBALS['page']->url(array('id' => $item->getId(), 'action' => 'edit'));
+		return $this->baseURL . $this->idName . '=' . $item->getId() . '&action=edit';
 	}
 	//-----------------------------------------------------------------------------
 }
